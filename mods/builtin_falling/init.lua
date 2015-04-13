bd5dcc382d1a8be8bf3fee83473a9c57c4b03ead
Dave Devil's <jtb1@voila.fr>
2015-04-14 01:34:42 +0200

    Protect about greaf by remplace node (during falling) ?
    I have add some "falling item" from all mods for more protection ...
    
    (Falling in protected zone is not avaible after )
    
    Maybe some mod are not activate in the server , maybe you need add some "--" for working (if the mod it not enable)

--- mods/builtin_falling/init.lua
+++ mods/builtin_falling/init.lua
@@ -11,9 +11,50 @@ PROTECT_WATER_REALTIME = 0 -- 0 for OFF , 1 for ON
 --
 -- List of your node need to rewirte
 --
+
+--Default
 add_falling_protect_item("default:sand")
 add_falling_protect_item("default:gravel")
+add_falling_protect_item("default:desert_sand") -- prevent cheat from falling node
 
+
+-- Snow
+--add_falling_protect_item("default:snow") -- another falling but maybe it's more realistique to not delete it ?
+add_falling_protect_item("default:snowblock")
+
+--Mod Pyramid
+add_falling_protect_item("tsm_pyramids:trap_2")
+
+--Mod SeaGravel
+add_falling_protect_item("seagravel:seagravel")
+add_falling_protect_item("seagravel:seagravel_cyan")
+add_falling_protect_item("seagravel:seagravel_magenta")
+add_falling_protect_item("seagravel:seagravel_lime")
+add_falling_protect_item("seagravel:seagravel_aqua")
+add_falling_protect_item("seagravel:seagravel_skyblue")
+add_falling_protect_item("seagravel:seagravel_redviolet")
+add_falling_protect_item("stairs:seagravel")
+add_falling_protect_item("stairs:seagravel_cyan")
+add_falling_protect_item("stairs:seagravel_magenta")
+add_falling_protect_item("stairs:seagravel_lime")
+add_falling_protect_item("stairs:seagravel_aqua")
+add_falling_protect_item("stairs:seagravel_skyblue")
+add_falling_protect_item("stairs:seagravel_redviolet")
+
+-- Mod Sea
+add_falling_protect_item("whiteshell:whiteshell")
+add_falling_protect_item("clams:sandalgae")
+add_falling_protect_item("clams:sandalgaeused")
+add_falling_protect_item("seawrecks:woodship")
+
+-- Mod SeaPlant
+add_falling_protect_item("seaplants:seaplantssandkelpgreen")
+add_falling_protect_item("seaplants:seaplantssandkelpbrown")
+add_falling_protect_item("seaplants:seaplantssandseagrassgreen")
+add_falling_protect_item("seaplants:seaplantssandseagrassred")
+
+
+-- Default liquid
 add_protected_bukket_liquid("bucket:bucket_lava","default:lava_source") -- lava bukket
 add_protected_bukket_liquid("bucket:bucket_water","default:water_source") -- water bukket
 
